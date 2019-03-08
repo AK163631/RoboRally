734e901c9ee69ea6383b156613cbdfb88bf76664
AsadKhan163631 <34480579+AsadKhan163631@users.noreply.github.com>
2019-03-08 19:54:30 +0000

    Add files via upload

---
+++ src/src/board/Gears.java
@@ -0,0 +1,20 @@
+package board;
+
+import main.Player;
+
+public class Gears extends BoardEntity {
+
+	public Gears(int x, int y, Board board, String repr) {
+		super(x, y, board, repr);
+	}
+
+	@Override
+	public void act(Player player, BoardEntity prevEntity) {
+		if (this.getFinalRepr() == "+") {
+			player.turnCW90(1);
+		} else {
+			player.turnACW90(1);
+		}
+	}
+
+}
